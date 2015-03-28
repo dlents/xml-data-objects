d2ad478da0dc37f5f160aab94f0b4504c4113a31
David Lents <david@lents.net>
2015-03-28 10:18:13 -0700

    Start testing new code

--- src/XmlDataObject.php
+++ src/XmlDataObject.php
@@ -28,25 +28,51 @@ class XmlDataObject {
      */
     public function __construct($name = '') {
         $this->name = $name;
-        $this->child = null;
-        $this->isLeaf = false;
+        // $this->child = null;
+        // $this->isLeaf = false;
         $this->_data = new ArrayObject( [ ], ArrayObject::ARRAY_AS_PROPS );
+        // print_r($this->_data);
     }
 
     public function __set($name, $value = null) {
-        $this->_data->offsetSet($name, $value);
-        if (is_scalar($value)) {
-            // $this->isLeaf = true;
+        if ($this->hasOwnProperty($name)) {
+            error_log( "XDO __set '$name' => '$value'" );
+            $this->$name = $value;
+        }
+        else {
+            error_log( "XDO __set DATA '$name' => '$value'" );
+            if (is_array($value)) {
+                $value = new ArrayObject($value);
+            }
+            $this->_data->$name = $value;
+        }
+    }
+
+    public function __get($name) {
+        if ( $this->hasOwnProperty( $name ) ) {
+            return $this->$name;
+        }
+        else {
+            return $this->_data->$name;
         }
     }
 
     public function setName($name) {
         $this->name = $name;
     }
+
+    public function getChildren() {
+        return $this->_data;
+    }
+
     /**
      * @return string
      */
     public function __toString() {
         return $this->name;
+    }
+
+    protected function hasOwnProperty($name) {
+        return in_array($name, get_object_vars($this));
     }
 }
